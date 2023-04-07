db1ed9f62c47342679c8e94f78880d944302e040
Ferdinand Calo <fercalo96@yahoo.de>
2023-04-07 22:41:29 +0200

    Update FCL.java

--- src/main/java/net/fexcraft/lib/mc/FCL.java
+++ src/main/java/net/fexcraft/lib/mc/FCL.java
@@ -1,6 +1,7 @@
 package net.fexcraft.lib.mc;
 
 import com.mojang.logging.LogUtils;
+import net.fexcraft.lib.common.Static;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
@@ -23,37 +24,30 @@ public class FCL {
     private static final Logger LOGGER = LogUtils.getLogger();
 
     public FCL(){
-        // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-        // Register the enqueueIMC method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
-        // Register the processIMC method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
-
-        // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
     }
 
     private void setup(final FMLCommonSetupEvent event){
-        LOGGER.info("FCL - PreInit");
+        //
     }
 
     private void enqueueIMC(final InterModEnqueueEvent event){
         // Some example code to dispatch IMC to another mod
-        InterModComms.sendTo("fcl", "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
+        //InterModComms.sendTo("fcl", "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
     }
 
     private void processIMC(final InterModProcessEvent event){
         // Some example code to receive and process InterModComms from other mods
-        LOGGER.info("Got IMC {}", event.getIMCStream().
-                map(m->m.messageSupplier().get()).
-                collect(Collectors.toList()));
+        //LOGGER.info("Got IMC {}", event.getIMCStream(). map(m->m.messageSupplier().get()).collect(Collectors.toList()));
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
     public void onServerStarting(ServerStartingEvent event){
-        LOGGER.info("FCL - Server Starting");
+        //LOGGER.info("FCL - Server Starting");
     }
 
     // You can use EventBusSubscriber to automatically subscribe events on the contained class (this is subscribing to the MOD
@@ -63,7 +57,7 @@ public class FCL {
 
         @SubscribeEvent
         public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent){
-            LOGGER.info("FCL - Block Registry");
+            //LOGGER.info("FCL - Block Registry");
         }
 
     }
